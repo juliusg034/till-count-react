bbc84ed43700a0c3b342c1c4413f4ba1fff94960
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-03-08 01:01:54 -0800

    add Switch component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Switch from './Components/Switch/Switch'
 
 function App() {
   return (
     <div className="App">
-      Hello, World! 
+     <Switch/> 
     </div>
   );
 }
