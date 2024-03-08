f6f296b33385198481a8fc54d3fe9b0090266233
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-03-08 00:57:26 -0800

    create Switch.js file

---
+++ src/Components/Switch.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Switch.css';
+
+function Switch () {
+  return (
+    <>
+      <input
+        className="react-switch-checkbox"
+        id={`react-switch-new`} 
+        type="checkbox"
+      />
+
+      <label
+        className="react-switch-label"
+        htmlFor={`react-switch-new`}
+      >
+        <span className={`react-switch-button`} />
+      </label>
+    </>
+  );
+};
+
+export default Switch;
