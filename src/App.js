7552495096330c1686bbeb1136914ce54238a4e9
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-03-08 16:29:39 -0800

    add on state
    use react useState to change styling, indicating on state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { useState } from 'react';
 import './App.css';
 import Switch from './Components/Switch/Switch'
 
 function App() {
+  const [value, setValue] = useState(false)
   return (
     <div className="App">
-     <Switch/> 
+      <Switch
+        isOn={value}
+        handleToggle={() => setValue(!value)}
+      /> 
     </div>
   );
 }
