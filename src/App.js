076acd1eef33501a3f4fc6ce0c12fda570dcdb07
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-03-09 14:51:11 -0800

    add heading
    Add a title and description for the website. Include basic color and outline for styling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ function App() {
   const [value, setValue] = useState(false)
   return (
     <div className="App">
-      <Switch
-        isOn={value}
-        handleToggle={() => setValue(!value)}
-      /> 
+      <heading>
+        <h1>DEPOSIT CALCULATOR</h1>
+        <h4>
+          Enter the <span class="description">number of bills</span> you have for each <br/>
+          denomination then click to refresh your total deposit.
+        </h4>
+      </heading>
+      
+      <Switch isOn={value} handleToggle={() => setValue(!value)}/> 
     </div>
   );
 }
