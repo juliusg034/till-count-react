b9bc93ee445d81dbe01b23cdbbed3d0d057a419b
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-03-14 15:36:32 -0700

    change ending till text

--- src/Components/BillList/BillList.js
+++ src/Components/BillList/BillList.js
@@ -21,12 +21,12 @@ function BillList ({ handleSubmit, values, deposit, total, endingTotal, depositT
         <h5 id="white">Ending Till</h5>
         <h5>Total = {endingTotal}</h5>
         <div class="line"></div>
-        <p>{values[100]} x $100</p>
-        <p>{values[50]} x $50</p>
-        <p>{values[20]} x $20</p>
-        <p>{values[10]} x $10</p>
-        <p>{values[5]} x $5</p>
-        <p>{values[1]} x $1</p>
+        <p>{values[100]*100} in $100</p>
+        <p>{values[50]*50} in $50</p>
+        <p>{values[20]*20} in $20</p>
+        <p>{values[10]*10} in $10</p>
+        <p>{values[5]*5} in $5</p>
+        <p>{values[1]} in $1</p>
         <p>{values[0.25]} in 25¢</p>
         <p>{values[0.10]} in 10¢</p>
         <p>{values[0.05]} in 5¢</p>
